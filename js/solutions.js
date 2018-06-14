ef011598b8cd7f6704f92889c971b0623d29a8cf
Badal Moradia <bamoradia@Badals-MacBook-Pro.local>
2018-06-14 18:43:31 -0500

    Commit 2 - Palindrome

--- js/solutions.js
+++ js/solutions.js
@@ -7,3 +7,23 @@ console.log("Javascript is linked");
 
 //3. A return statement will not print anything anywhere by itselt. It will pass the value to the place where the function was actually called. A console.log inside of a function will actually print that value to the console.
 
+
+
+//Palindrome
+
+const palindrome = (input) => {
+	input = input.toLowerCase();
+	const splitString = input.split('');
+	const reversedArray = splitString.reverse();
+	const reversedWord = reversedArray.join('')
+	if (reversedWord === input) {
+		return true
+	}
+	return false
+}
+
+console.log(palindrome('Radar'));
+console.log(palindrome('borscht'));
+
+
+
