4a0426636ff028e80f615cb589c49b0840661020
Badal Moradia <bamoradia@Badals-MacBook-Pro.local>
2018-06-14 18:34:09 -0500

    Commit 1 - Verbal questions

---
+++ js/solutions.js
@@ -0,0 +1,9 @@
+console.log("Javascript is linked");
+
+//Badal Moradia HW 3 Answers
+
+//2. A parameter is the variable/input that is setup during a function declaration
+// An argument is the input into the function when the function is being called.
+
+//3. A return statement will not print anything anywhere by itselt. It will pass the value to the place where the function was actually called. A console.log inside of a function will actually print that value to the console.
+
